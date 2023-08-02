beb378338e03b109d840bdd743e3cb954b67ccfc
MonaLisaFrank <126209931+RedactdName@users.noreply.github.com>
2023-08-02 14:02:41 -0600

    JQuery logic(js) of first to-do-.saveBtn

--- cpyscript.js
+++ cpyscript.js
@@ -8,17 +8,32 @@ $(function () {
     // function? How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
+    $(".saveBtn").on("click",function () {
+      var value = $(this).siblings(".description").val();
+      var hourKey = $(this).parent().attr("id");
+      localStorage.setItem(hourKey, value)
+    } )
     //
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
+    // current hour in 24-hour time? dayjs/momentjs
     //
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-    //
+      $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+      $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+      $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+      $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+      $("#hour-1 .description").val(localStorage.getItem("hour-1"));
+      $("#hour-2 .description").val(localStorage.getItem("hour-2"));
+      $("#hour-3 .description").val(localStorage.getItem("hour-3"));
+      $("#hour-4 .description").val(localStorage.getItem("hour-4"));
+      $("#hour-5 .description").val(localStorage.getItem("hour-5"))  
+          
+
     // TODO: Add code to display the current date in the header of the page.
   });
 // WHEN I open the planner
