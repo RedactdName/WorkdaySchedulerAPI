e9dec0f89c47e20bb38549dddbe143dd7b8189c2
MonaLisaFrank <126209931+RedactdName@users.noreply.github.com>
2023-08-03 20:52:17 -0600

    adding more js for past,present,future

--- cpyscript.js
+++ cpyscript.js
@@ -1,6 +1,3 @@
-// TODO: Add code to display the current date in the header of the page.
-var today = dayjs().format('llll');
-$(".currentDay").html(today);
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
@@ -43,27 +40,30 @@ $(function () {
         
       }
     })
-}
-    
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-
-    //Get item from local storage 
-    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-    $("#hour-1 .description").val(localStorage.getItem("hour-1"));
-    $("#hour-2 .description").val(localStorage.getItem("hour-2"));
-    $("#hour-3 .description").val(localStorage.getItem("hour-3"));
-    $("#hour-4 .description").val(localStorage.getItem("hour-4"));
-    $("#hour-5 .description").val(localStorage.getItem("hour-5"))  
-    
-    
-    workdayHours();
-  });
+  }
+  // TODO: Add code to display the current date in the header of the page.
+  var today = dayjs().format('llll');
+  $(".currentDay").html(today);
+  
+  //
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+  
+  //Get item from local storage 
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-1 .description").val(localStorage.getItem("hour-1"));
+  $("#hour-2 .description").val(localStorage.getItem("hour-2"));
+  $("#hour-3 .description").val(localStorage.getItem("hour-3"));
+  $("#hour-4 .description").val(localStorage.getItem("hour-4"));
+  $("#hour-5 .description").val(localStorage.getItem("hour-5"))  
+  
+  
+  workdayHours();
+});
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
 // WHEN I scroll down
