fae20d92dfdfd87e0dc14d98d0dc0297c705c5aa
MonaLisaFrank <126209931+RedactdName@users.noreply.github.com>
2023-08-05 11:30:04 -0600

    change format for day of the week

--- assets/cpyscript.js
+++ assets/cpyscript.js
@@ -5,7 +5,7 @@ $(function () {
   $("#currentDay").text(dayjs().format("dd, MMM D, YYYY hh:mm"))
 // Refreshes the date/time display after 60sec
   setInterval(function() {
-    $("#currentDay").text(dayjs().format("ddd, MMM D, YYYY hh:mm"))
+    $("#currentDay").text(dayjs().format("dddd, MMM D, YYYY hh:mm"))
   },60000)
 // Enables save button for each hour/row 
   $(".saveBtn").on("click",function () {
